1872feecd276f5675bf59e92bf652a70e0f2c69c
Greg Turnquist <gturnquist@gopivotal.com>
2013-07-31 11:04:26 -0400

    Write draft of getting started guide with before/after demo

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,7 +1,13 @@
 package hello;
 
+import java.util.Arrays;
+
+import javax.servlet.MultipartConfigElement;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -12,8 +18,21 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @ComponentScan
 public class Application {
 	
+	@Bean
+	MultipartConfigElement multipartConfigElement() {
+		return new MultipartConfigElement("");
+	}
+	
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+		
+		System.out.println("Let's inspect the beans provided by Spring Boot:");
+		
+		String[] beanNames = ctx.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		for (String beanName : beanNames) {
+			System.out.println(beanName);
+		}
 	}
 
 }
--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-
+	
 	@RequestMapping("/")
 	public @ResponseBody String index() {
 		return "Greetings from Spring Boot!";
